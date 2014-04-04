79244c763b8117785a170520668a93404aba5d34
Ken Luk <knnyluk@gmail.com>
2014-04-03 20:04:49 -0500

    initialized with pg database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Questions::Application.config.secret_key_base = '824d02f672a94a57a11d4962a21b1b8f5cad7678e66c95718dc8343f5e8c1570a74693342c91027a2167296a7a2650bbd8bbf9ac90ca3e42302823c4c96f93a2'
